641b0d36cc97c518cf0545bf16b5e0a1ac66b3da
edgarUser <edgar.lopez.isc@gmail.com>
2024-03-08 16:52:52 -0600

    Adding checktyle configuration

--- src/main/java/org/ideation/poc/PocApplication.java
+++ src/main/java/org/ideation/poc/PocApplication.java
@@ -3,11 +3,18 @@ package org.ideation.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * PocApplication class.
+ */
 @SpringBootApplication
 public class PocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
-	}
+  /**
+   * Spring Boot main class.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(PocApplication.class, args);
+  }
 
 }
---
+++ src/main/java/org/ideation/poc/controller/PocController.java
@@ -0,0 +1,20 @@
+package org.ideation.poc.controller;
+
+import java.util.List;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ResController class.
+ */
+@RestController
+public class PocController {
+
+  @GetMapping(path = "/details")
+  public ResponseEntity<List<String>> getDetails() {
+    return new ResponseEntity<>(List.of("Hello"), HttpStatusCode.valueOf(200));
+  }
+
+}
